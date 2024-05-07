501fdd25fd2fe9845d5760b8e0feea50f40fb146
cris2804 <milagros.ruiz.a@uni.pe>
2024-05-07 14:02:00 -0500

    Activdiad RGR +TDR iniada

---
+++ Actividad5-TDD/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+b
+2TDD-1/src/test/java/com/wordz/PlaceholderTest.java,8\7\876fb184798f038632523193a024b619fa8396b7
--- Actividad5-TDD/TDD-1/src/test/java/com/wordz/WordTest.java
+++ Actividad5-TDD/TDD-1/src/test/java/com/wordz/WordTest.java
@@ -8,9 +8,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class WordTest {
     @Test
     public void oneIncorrectLetter() {
+        //arrange
         var word = new Word("A");
         var score = word.guess("Z"); //guess = adivinar intento de adivinar
+       //act
         Letter result = score.letter(0);
+        //assert
         assertThat(result).isEqualTo(Letter.INCORRECT);
 
     }
