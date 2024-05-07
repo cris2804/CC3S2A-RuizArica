7bc1e292253122ce41b53a86e555e8fa2873bb16
cris2804 <milagros.ruiz.a@uni.pe>
2024-05-07 01:40:49 -0500

    Actividad tessting terminada

--- Actividad1-Testing/src/test/java/org/example/ClientTest.java
+++ Actividad1-Testing/src/test/java/org/example/ClientTest.java
@@ -2,9 +2,13 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-public class ClientTest {
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+ class ClientTest {
+    //Arrange
     private Address address = new Address("street A");
-    private Address otherAddress = new Address("street B");
+    private Address otherAddress = new Address("street A");
     private  Client client ;
     @BeforeEach //metodo que se ejecuta antes de cada prueba
     void setUp() {//setup sirve para inicializar los atributos de la clase
@@ -13,18 +17,29 @@ public class ClientTest {
     @Test
 
     void afterCreationShouldHaveNoAddress() {
-        //Completa
+        //Act
+        int num_direcciones = client.getAddresses().size();
+        //Assert
+        assertEquals(0,num_direcciones);//devuelve el tamanio de la lista
     }
 
     @Test
     void shouldAllowToAddAddress() {
-        //Completa
+        //Act
         client.addAddress(address);
+        int num_direcciones = client.getAddresses().size();
+        //Assert
+        assertEquals(1, num_direcciones);
     }
     @Test
     void shouldAllowToAddSameAddresses() {
+        //Act
         client.addAddress(address);
         client.addAddress(otherAddress);
+        String direccion1_cliente = client.getAddresses().get(0).getStreet();
+        String direccion2_cliente = client.getAddresses().get(1).getStreet();
+        //Assert
+        assertEquals(direccion2_cliente, direccion1_cliente);
     }
 
 
